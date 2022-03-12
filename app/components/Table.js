66b3db272af57ed1e2673103c9eedd8e9e1a4fbf
Adylan <adylan.dev@gmail.com>
2022-03-12 17:31:48 +0800

    add customized tables

---
+++ app/components/Table.js
@@ -0,0 +1,51 @@
+import { styled } from '@mui/material/styles';
+import Table from '@mui/material/Table';
+import TableBody from '@mui/material/TableBody';
+import TableCell, { tableCellClasses } from '@mui/material/TableCell';
+import TableContainer from '@mui/material/TableContainer';
+import TableHead from '@mui/material/TableHead';
+import TableRow from '@mui/material/TableRow';
+import Paper from '@mui/material/Paper';
+
+const StyledTableCell = styled(TableCell)(({ theme }) => ({
+    [`&.${tableCellClasses.head}`]: {
+        backgroundColor: theme.palette.common.black,
+        color: theme.palette.common.white,
+    },
+    [`&.${tableCellClasses.body}`]: {
+        fontSize: 14,
+    },
+}));
+
+const StyledTableRow = styled(TableRow)(({ theme }) => ({
+    '&:nth-of-type(odd)': {
+        backgroundColor: theme.palette.action.hover,
+    },
+    // hide last border
+    '&:last-child td, &:last-child th': {
+        border: 0,
+    },
+}));
+
+
+export default function CustomizedTables({ columns, data }) {
+    console.log(columns)
+    return (
+        <TableContainer component={Paper}>
+            <Table sx={{ minWidth: 700 }} aria-label="customized table">
+                <TableHead>
+                    <TableRow>
+                        {columns.map((column) => <StyledTableCell>{column}</StyledTableCell>)}
+                    </TableRow>
+                </TableHead>
+                <TableBody>
+                    {data.map((row) => (
+                        <StyledTableRow key={row.name}>
+                            {columns.map((column) => <StyledTableCell>{row[column]}</StyledTableCell>)}
+                        </StyledTableRow>
+                    ))}
+                </TableBody>
+            </Table>
+        </TableContainer>
+    );
+}
--- app/pages/drivers/index.js
+++ app/pages/drivers/index.js
@@ -1,11 +1,13 @@
 import { usePaginatedQuery, useQuery, useRouter } from "blitz"
 import getDrivers from "app/drivers/queries/getDrivers"
+import CustomizedTables from "app/components/Table"
 
 const ITEMS_PER_PAGE = 100
 
 const DriversPage = (props) => {
     const router = useRouter()
     const page = Number(router.query.page) || 0
+    const columns = ["driverName", "phoneNumber", "idCard", "driverLicense"]
 
     const [{ drivers, hasMore }] = usePaginatedQuery(getDrivers, {
         orderBy: { id: "asc" },
@@ -19,14 +21,7 @@ const DriversPage = (props) => {
                 {drivers.length === 0 && (
                     <p>No drivers found</p>
                 )}
-                {drivers.map(driver => (
-                    <div>
-                        <p>{driver.driverName}</p>
-                        <p>{driver.phoneNumber}</p>
-                        <p>{driver.idCard}</p>
-                        <p>{driver.driverLicense}</p>
-                    </div>
-                ))}
+                <CustomizedTables columns={columns} data={drivers} />
             </main>
         </div>
     </>
