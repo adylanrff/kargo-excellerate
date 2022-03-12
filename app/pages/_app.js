75fe88134f15523ad460172b72572fbacd65ffc6
edwardtanoto <edwardt.dev@gmail.com>
2022-03-12 14:40:54 +0700

    [wip]

--- app/pages/_app.js
+++ app/pages/_app.js
@@ -1,10 +1,12 @@
 import './styles.css'
 import Navbar from '../components/Navbar'
+import { StyledEngineProvider } from '@mui/material/styles';
+
 
 function MyApp({ Component, pageProps }) {
-  return (<>
+  return (<StyledEngineProvider injectFirst>
     <Navbar />
     <Component {...pageProps} />
-  </>)
+  </StyledEngineProvider>)
 }
 export default MyApp
